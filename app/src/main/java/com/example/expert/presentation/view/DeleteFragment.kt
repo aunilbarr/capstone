d1077c1970f37835614141781dd5da8da0c30c6d
aunilbarr <61440644+aunilbarr@users.noreply.github.com>
2021-07-31 23:26:11 +0700

    capstone2

--- app/src/main/java/com/example/expert/presentation/view/DeleteFragment.kt
+++ app/src/main/java/com/example/expert/presentation/view/DeleteFragment.kt
@@ -10,16 +10,6 @@ import androidx.fragment.app.DialogFragment
 import com.example.expert.R
 import kotlinx.android.synthetic.main.fragment_delete.*
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [DeleteFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class DeleteFragment : DialogFragment(), View.OnClickListener {
 
     companion object {
--- core/src/main/java/com/example/expert/core/di/coreModule.kt
+++ core/src/main/java/com/example/expert/core/di/coreModule.kt
@@ -6,10 +6,16 @@ import com.example.expert.core.data.source.remote.network.ApiService
 import com.example.expert.core.domain.repository.IMovieRepository
 import com.example.expert.core.domain.repository.MovieInteractor
 import com.example.expert.core.domain.usecase.MovieUseCase
+import net.sqlcipher.database.SupportFactory
+import net.sqlcipher.database.SQLiteDatabase
+import okhttp3.CertificatePinner
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import org.koin.android.ext.koin.androidContext
 import org.koin.dsl.module
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
 
 val useCaseModule = module {
     factory<MovieUseCase> { MovieInteractor(get()) }
@@ -18,14 +24,33 @@ val useCaseModule = module {
 val databaseModule = module {
     factory { get<com.example.expert.core.data.source.local.room.movieDatabase>().movieDao() }
     single {
+        val passphrase = SQLiteDatabase.getBytes("expert".toCharArray())
+        val factory = SupportFactory(passphrase)
         Room.databaseBuilder(
             androidContext(),
             com.example.expert.core.data.source.local.room.movieDatabase::class.java, "movie.db"
-        ).fallbackToDestructiveMigration().build()
+        )
+            .fallbackToDestructiveMigration()
+            .openHelperFactory(factory)
+            .build()
     }
 }
 
 val networkModule = module {
+    single {
+        val hostname = "api.themoviedb.org"
+        val certificatePinner = CertificatePinner.Builder()
+            .add(hostname, "sha256/+vqZVAzTqUP8BGkfl88yU7SQ3C8J2uNEa55B7RZjEg0=")
+            .add(hostname, "sha256/JSMzqOOrtyOT1kmau6zKhgT676hGgczD5VMdRMyJZFA=")
+            .add(hostname, "sha256/++MBgDH5WGvL9Bcn5Be30cRcL0f5O+NyoXuWtQdX1aI=")
+            .build()
+        OkHttpClient.Builder()
+            .addInterceptor(HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY))
+            .connectTimeout(120, TimeUnit.SECONDS)
+            .readTimeout(120, TimeUnit.SECONDS)
+            .certificatePinner(certificatePinner)
+            .build()
+    }
     single {
         val retrofit = Retrofit.Builder()
             .baseUrl("https://api.themoviedb.org/3/movie/")
